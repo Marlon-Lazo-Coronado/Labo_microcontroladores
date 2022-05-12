dfd4de30a1e5be05d56a9646d032a25237fcd242
Marlon-Lazo-Coronado <mlazocoronado@hotmail.com>
2022-05-11 22:54:53 -0700

    we made a unit test for external and timers interruptions

--- blink.c
+++ blink.c
@@ -1,15 +1,46 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
-int main(void)
-{
-  DDRB = 0x08; //Configuracion del puerto
+int request_botton = 0 ;
 
-  //Parpadear
-  while (1) {
-    PORTB = 0x00; //PORTB &= ~(1 << PB3); //Esto se puede hacer tambien asi
-    _delay_ms(500); 
-    PORTB = 0x08; //PORTB |=  (1 << PB3); //Esto se puede hacer tambien asi
-    _delay_ms(500); 
+/* we lisening the interrup in D0 */
+ISR (PCINT2_vect){
+  request_botton = 1 ;
+}
+
+/* we lisening the timer interruption */
+ISR (TIMER0_OVF_vect)
+{
+  if (request_botton == 1 )
+
+      PORTB |= ( 1 <<PB0)|( 1 <<PB1)|( 1 <<PB2);    
+}
+
+void  settings (){
+  /* we set the outputs */
+  DDRB |= ( 1 <<PB0) | ( 1 <<PB1) | ( 1 <<PB2);
+
+  /* we set the interrup for the D0 input */
+  GIMSK |= ( 1 << PCIE2);
+  PCMSK2 = 0b00000001 ;
+  
+  /* settings for timer interrup */
+  TCCR0A= 0x00 ;   //couters in normal mode
+  TCCR0B= 0x00 ;
+  TCCR0B |= ( 1 <<CS00)|( 1 <<CS02);   // prescaling in 1024
+  sei (); // we call global interrups
+  TCNT0= 0 ;  
+  TIMSK|=( 1 <<TOIE0); // we set the timer0
+}
+
+int main( )
+{
+  
+
+  /* llamada de funcion */
+  settings ();
+
+  while( 1 ) {
   }
 }
